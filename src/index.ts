617e462d434b3f0cd104f88cd728eb4d0e91fded
KelvinMitaki <mitakikelvin1@gmail.com>
2020-09-07 17:47:16 +0300

    added a sorter.ts

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,4 @@
-class Sorter {
-  constructor(public collection: number[]) {}
-  sort(): void {
-    const { length } = this.collection;
-
-    for (let i = 0; i < length; i++) {
-      for (let j = 0; j < length - i - 1; j++) {
-        if (this.collection[j] > this.collection[j + 1]) {
-          const leftHand = this.collection[j];
-          this.collection[j] = this.collection[j + 1];
-          this.collection[j + 1] = leftHand;
-        }
-      }
-    }
-  }
-}
+import { Sorter } from "./Sorter";
 
 const sorter = new Sorter([9, 24, 5, 456, -1, 2, 3, 4, 342, 3121]);
 sorter.sort();

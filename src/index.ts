570e3e72039d95000c4af652d9b1eb8ed2450723
KelvinMitaki <mitakikelvin1@gmail.com>
2020-09-07 22:21:08 +0300

    changed sorter to be abstract class

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,32 @@
 import { Sorter } from "./Sorter";
 import { NumbersCollection } from "./NumbersCollection";
 import { CharactersCollection } from "./CharactersCollection";
+import { LinkedList } from "./LinkedList";
 
-const numbersCollection = new NumbersCollection([4, 342, 3121]);
+const numbersCollection = new NumbersCollection([
+  9,
+  24,
+  5,
+  456,
+  -1,
+  2,
+  3,
+  4,
+  342,
+  3121
+]);
+
 const charactersCollection = new CharactersCollection(
   "kjhKJHKjhkjkjhkjGHSAJKXxxxhghkaahfsjgjhs"
 );
-const sorter = new Sorter(charactersCollection);
+
+const linkedList = new LinkedList();
+
+linkedList.add(-2);
+linkedList.add(50);
+linkedList.add(73);
+linkedList.add(508);
+
+const sorter = new Sorter(linkedList);
 sorter.sort();
-console.log(charactersCollection.data);
+console.log(linkedList.print());
